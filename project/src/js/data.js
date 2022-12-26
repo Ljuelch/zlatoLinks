4f0fa360273d96f01d57025ccdc4841dd31b8bcc
Ljuelch <leojuelch@yahoo.de>
2022-12-26 04:11:13 +0100

    order changes, song hovers

---
+++ project/src/js/data.js
@@ -0,0 +1,8 @@
+let songList = {
+	"001": {
+		title: "Blätter Braun",
+		cover: "1BlätterBraun.jpg",
+		apple: "<iframe allow=\"autoplay *; encrypted-media *; fullscreen *; clipboard-write\" frameborder=\"0\" height=\"200\" style=\"width:100%;max-width:660px;overflow:hidden;background:transparent; padding-top: 30px\" sandbox=\"allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation\" src=\"https://embed.music.apple.com/us/album/bl%C3%A4tter-braun-feat-nadivi/1659680638?i=1659680640\"></iframe>",
+		spotify: "<iframe style=\" display: none; border-radius:12px\" src=\"https://open.spotify.com/embed/track/6x82tlNHqx7Ve2QZONeTJ3?utm_source=generator\" width=\"100%\" height=\"152\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>",
+	},
+}
---
+++ project/src/js/style.js
@@ -0,0 +1,11 @@
+$(".go-to").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#page-content").offset().top
+	}, 2000);
+});
+
+// $(".spotify").click(function (){
+// 	window.open('google.com', '_blank');
+// });
+
+
