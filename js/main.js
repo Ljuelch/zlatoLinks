f307e48dd0d60f4a0e893d004f0732985af4324f
Ljuelch <leojuelch@yahoo.de>
2023-01-30 02:45:41 +0100

    resp slider

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ $(document).ready(()=>{
 	$(".go-to").click(()=>
 		$('html, body').animate({
 			scrollTop: $("#page-content").offset().top
-		}, 2000)
+		}, 100)
+	)
+
+	$(".go-to-mobile").click(()=>
+		$('html, body').animate({
+			scrollTop: $("#sec-vid-helper").offset().top
+		}, 100)
 	)
 	
 	AOS.init({
